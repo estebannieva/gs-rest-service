3d9fa7903b83b81127395baff3a74c0aaa691392
Esteban Nieva <estebannieva.dev@gmail.com>
2025-06-01 02:19:46 +0100

    feat(model): add Price entity

---
+++ src/main/java/com/example/restservice/model/Price.java
@@ -0,0 +1,54 @@
+package com.example.restservice.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "prices")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Price {
+
+    @Id
+    @GeneratedValue
+    @Setter(AccessLevel.NONE)
+    private UUID id;
+
+    @NotNull
+    @Column(name = "brand_id", nullable = false)
+    private Integer brandId;
+
+    @NotNull
+    @Column(name = "product_id", nullable = false)
+    private Integer productId;
+
+    @NotNull
+    @Column(name = "start_date", nullable = false)
+    private LocalDateTime startDate;
+
+    @NotNull
+    @Column(name = "end_date", nullable = false)
+    private LocalDateTime endDate;
+
+    @NotNull
+    @Column(name = "price_list", nullable = false)
+    private Integer priceList;
+
+    @NotNull
+    @Column(name = "priority", nullable = false)
+    private Integer priority;
+
+    @NotNull
+    @Column(name = "price", nullable = false)
+    private BigDecimal price;
+
+    @NotNull
+    @Column(name = "curr", nullable = false)
+    private String currency;
+}

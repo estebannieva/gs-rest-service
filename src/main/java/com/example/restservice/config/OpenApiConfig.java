3d1c2ca388843602d4b023dc5cb918427bd12a74
Esteban Nieva <estebannieva.dev@gmail.com>
2025-06-03 18:58:58 +0100

    feat(config): add OpenAPI configuration class

---
+++ src/main/java/com/example/restservice/config/OpenApiConfig.java
@@ -0,0 +1,27 @@
+package com.example.restservice.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Value("${spring.application.name}")
+    private String appName;
+
+    @Value("${spring.application.version}")
+    private String appVersion;
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title(appName)
+                        .version(appVersion)
+                        .description("Pricing Rate Inquiry API Documentation")
+                );
+    }
+}
